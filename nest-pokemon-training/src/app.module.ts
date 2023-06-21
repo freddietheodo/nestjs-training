01855c72079948824cc4fd7453ad57df6d377a7b
freddietheodo <freddiew@theodo.co.uk>
2023-06-21 15:41:05 +0100

    feat: getPokemon

--- nest-pokemon-training/src/app.module.ts
+++ nest-pokemon-training/src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { PokemonService } from "./pokemon/pokemon.service";
+import { PokemonController } from "./pokemon/pokemon.controller";
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PokemonController],
+  providers: [AppService, PokemonService],
 })
 export class AppModule {}
