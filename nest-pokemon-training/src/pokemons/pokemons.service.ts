a92a877f0440ce4c05012872622da855814e8710
freddietheodo <freddiew@theodo.co.uk>
2023-07-19 11:27:22 +0100

    feat: unit-test

--- nest-pokemon-training/src/pokemons/pokemons.service.ts
+++ nest-pokemon-training/src/pokemons/pokemons.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { CreatePokemonDto } from "./dto/create-pokemon.dto";
 import { UpdatePokemonDto } from "./dto/update-pokemon.dto";
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
 export class PokemonsService {
